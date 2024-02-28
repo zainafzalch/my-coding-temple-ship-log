117214dbfb8afc4bc21e7cb1a2272c35f9d3bea9
zainafzalch <zainafzal6610b@gmail.com>
2024-02-27 22:15:20 -0500

    using pretty print with nvim

--- adventure_log.py
+++ adventure_log.py
@@ -1,5 +1,6 @@
 def greet_coding_temple():
     print("Ahoy, coding temple navigators! Ready for todays adventure?") 
+    __import__('pprint').pprint("Hello world!")
 
 if __name__ == "__main__":
     greet_coding_temple() 
