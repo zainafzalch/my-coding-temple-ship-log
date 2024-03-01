06012519b2a10a218601867b7d11256c51fd913c
zainafzalch <zainafzal6610b@gmail.com>
2024-03-01 13:11:40 -0500

    adding changes

--- adventure_log.py
+++ adventure_log.py
@@ -1,6 +1,11 @@
 def greet_coding_temple():
-    print("Ahoy, coding temple navigators! Ready for todays adventure?") 
-    __import__('pprint').pprint("Hello world!")
+    print("Ahoy, coding temple navigators! Ready for todays adventure?")
+    __import__("pprint").pprint("Hello world!")
+
+
+def hello_world():
+    print("WTF is this shit")
+
 
 if __name__ == "__main__":
-    greet_coding_temple() 
+    greet_coding_temple()
